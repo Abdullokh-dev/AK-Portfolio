57d900058a16f533da73d659681eba8d76a9d6b6
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2023-03-05 16:37:11 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+import Header from "./componennts/Header.vue";
+</script>
+
+<template>
+  <div class="container-fluid" id="mol">
+    <Header />
+  </div>
+</template>
+
+<style>
+* {
+  font-family: 'Jost', sans-serif;
+  font-weight: 700;
+}
+
+.dark {
+  background-color: #111A20;
+  --my-deep-gray: #293241;
+  --my-dark: #182228;
+  --my-text-color: white;
+  --bg-color: #232C41;
+  --my-orange: #FFA97A;
+  --header-color: #17232B;
+}
+
+.light {
+  background-color: white;
+  --my-deep-gray: #293241;
+  --my-dark: #182228;
+  --my-text-color: #293241;
+  --bg-color: #232C41;
+  --my-orange: #FFA97A;
+  --header-color: #9ba2a8;
+}
+
+</style>

96cfe4b452a38d8b6316cd89c541eda4be934090
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2023-03-15 16:49:59 +0500

    add some changes

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,15 +3,15 @@ import Home from "../pages/Home.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  mode: 'history',
+  base: import.meta.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
       component: Home
     },
     {
       path: '/contact',
-      name: 'contact',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.

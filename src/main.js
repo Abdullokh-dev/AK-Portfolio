70a7f1f0be8d8287fd031a426d7a4a48e8fe6b5d
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2023-04-09 19:41:01 +0500

    initial i18n commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./plugins/router.js";
+import { createI18n } from 'vue-i18n';
+import en from "./locales/en.json"
+import ru from "./locales/ru.json";
 
-createApp(App).use(router).mount('#app')
+const i18n = createI18n({
+  locale: 'en',
+  allowComposition: true,
+  messages: {
+    en: en,
+    ru: ru
+  }
+})
+
+createApp(App).use(i18n).use(router).mount('#app')

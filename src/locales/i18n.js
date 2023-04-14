3063ac9dd420e028a8be36d70e0826261303d114
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2023-04-15 03:10:52 +0500

    improvement translation

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -2,9 +2,18 @@ import { createI18n } from "vue-i18n";
 import en from "./en.json";
 import ru from "./ru.json";
 
+const saveLanguage = () => {
+  if (localStorage.getItem('language') === null) {
+    return 'Рус'
+  } else {
+    return localStorage.getItem('language')
+  }
+}
+
 const i18n = createI18n({
-  locale: 'Рус',
+  locale: saveLanguage(),
   allowComposition: true,
+  legacy: false,
   messages: {
     Eng: en,
     Рус: ru

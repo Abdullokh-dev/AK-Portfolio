ce7d764da6b1ac57f18518fe9ec4fe2fd09e4568
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-06-02 06:25:31 +0300

    routing

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,20 +1,21 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import Home from "../pages/Home.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   mode: 'history',
   base: import.meta.env.BASE_URL,
   routes: [
+    {
+      path: '/:pathMatch(.*)*',
+      component: Home
+    },
     {
       path: '/',
       component: Home
     },
     {
       path: '/contact',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../pages/Contact.vue')
     }
   ]

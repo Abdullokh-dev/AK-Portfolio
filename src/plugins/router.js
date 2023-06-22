9846f449698d4fe009fba6bbd69bb7038cb25b5b
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-06-22 13:06:02 +0300

    edit routing

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,8 +3,8 @@ import Home from "../pages/Home.vue"
 
 const router = createRouter({
   history: createWebHistory(),
-  mode: 'history',
-  base: import.meta.env.BASE_URL,
+  // mode: 'history',
+  // base: import.meta.env.BASE_URL,
   routes: [
     {
       path: '/',

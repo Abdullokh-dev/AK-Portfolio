86cb05f8397c5850ab2b4cb3db46cc7d2215f08b
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-07-13 22:09:35 +0300

    improvement

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      // Proxy configuration if needed
-    },
-    middlewareMode: true,
-    fs: {
-      strict: true,
-    },
-  }
 })
